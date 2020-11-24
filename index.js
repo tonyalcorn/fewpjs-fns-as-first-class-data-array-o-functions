7ccd8f0b5f5a3182d9da56e7e9aae920846db387
tonyalcorn <xmastony@gmail.com>
2020-11-23 17:25:36 -0800

    commit tree 4f3fa56abdf631dfedb468c34ced32ba8d9320ff

---
+++ index.js
@@ -0,0 +1,46 @@
+
+function wakeDog(dogName, dogBreed) {
+    const string = `Wake ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function leashDog(dogName, dogBreed) {
+    const string = `Leash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function walkToPark(dogName, dogBreed) {
+    const string = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    const string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function walkHome(dogName, dogBreed) {
+    const string = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function unleashDog(dogName, dogBreed) {
+    const string = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function exerciseDog(dogName, dogBreed) {
+    return routine.map(thing => thing(dogName, dogBreed))
+
+    // for (let i = 0; i < routine.length; i++) {
+    //     return routine[i](dogName, dogBreed);
+    
+}
